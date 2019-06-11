e18ab53c4640c4b2971695882089ec0f5d46ac18
Ayushgame <44751335+Ayushgame@users.noreply.github.com>
2019-06-11 11:12:20 +0530

    extraction of tweets from twitter

---
+++ getting_started.py
@@ -0,0 +1,21 @@
+import tweepy
+from tweepy import OAuthHandler
+
+
+consumer_key="***********************************"
+consumer_secret="*******************************"
+access_token="************************************"
+access_token_secret="***************************" 
+
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+public_tweets = api.home_timeline()
+
+for tweet in public_tweets:
+    
+    u=tweet.text
+    u=u.encode('unicode-escape').decode('utf-8')
+    print(u)
